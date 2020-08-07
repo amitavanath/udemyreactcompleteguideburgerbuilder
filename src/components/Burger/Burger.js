cfc9859353ae21fbaa61283b8637c0b91cd937cf
amitava nath <amitavanath@github.com>
2020-08-07 22:13:54 +0530

    commit changes

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    
+//    const transformedIngredients = Object.keys(props.ingredients)
+//    .map((igkey) => {
+//        return console.log(igkey + props.ingredients[igkey]);
+//    });
+    
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map((igkey) => {
+        return [...Array(props.ingredients[igkey])].map((_, i) => {
+            return <BurgerIngredient key={igkey + i} type={igkey} />;
+        });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>"Add somethings !!!"</p>;
+    }
+
+    console.log(transformedIngredients);
+    
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+};
+
+export default burger;
